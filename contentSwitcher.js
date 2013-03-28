fdb5a67fa71f49abbdf9d33e0c3150361b6e4696
Deric Mendez <mendezd@digitalev.com>
2013-03-28 12:41:23 -0500

    init

---
+++ contentSwitcher.js
@@ -0,0 +1,156 @@
+(function($) {
+
+    var defaults = {
+        'header'       : '> h3', 
+        'activeIndex'  : 0, 
+        'onChange'     : null,
+        'headerAsHtml' : false,
+        'toggleHeaders': false,
+        'tabLabel'     : true,
+        'classNames': {
+            'content'  : 'tabbed-content',
+            'active'   : 'active',
+            'tabs'     : 'tabs',
+            'tabItems' : 'tab-items',
+            'tabLabel' : 'tab-label button button-dropdown',
+            'headers'  : 'header-inactive'
+        }
+    };
+
+    function ContentSwitcher(element, settings) {
+
+        var $container = $(element);
+        var $panels = $container.children();
+        var $tabListWrapper = $('<div />');
+        var $tabList = $('<ul></ul>');
+        var $tabLabel = $('<a />', {
+            'href' : "javascript:void(0);",
+            'class': settings.classNames.tabLabel
+        });
+        var tabs = [];
+        var activeHeader;
+
+        var removeActiveState = function() {
+            $container.removeClass(settings.classNames.active); 
+        };
+
+        var labelToggleActive = function(event) {
+            event.stopPropagation();
+            $container.toggleClass(settings.classNames.active);
+            return false;
+        };
+
+        var destroy = function() {
+            $tabListWrapper.remove();
+        };
+
+        var parsePanels = function(index) {
+            var $panel = $(this);
+            var $header = $panel.find(settings.header);
+            var $tabLink = $('<a />', {
+                'href': "#" + $panel.attr("id"),
+            });
+            var $tabLi = $('<li />', {
+                'class': $panel.attr('class') || ''
+            });
+
+            var isCurrentHeader = function() {
+                return (activeHeader != undefined) && activeHeader == index;
+            };
+
+            var tabClick = function(event) {
+                var $this = $(this);
+                var activeClass = settings.classNames.active;
+
+                $panels
+                    .removeClass(activeClass);
+                
+                if ( settings.toggleHeaders && !isCurrentHeader())
+                    $panel.removeClass(settings.classNames.headers);
+
+                $tabList
+                    .children()
+                    .removeClass(activeClass);
+
+                $panel
+                    .addClass(activeClass);
+
+                $tabLi
+                    .addClass(activeClass);
+                
+                if ( settings.tabLabel)
+                    $tabLabel
+                        .text($this.text());
+
+                if(settings.onChange)
+                    settings.onChange.call(this, $panel);
+
+                activeHeader = index;
+
+                event.preventDefault();
+            };
+
+            $header.on('click', function() {
+
+                if ( settings.toggleHeaders && isCurrentHeader())
+                    $panel.toggleClass(settings.classNames.headers);
+
+                $tabLink.click();
+                
+            });
+
+            $tabLink.html(
+                settings.headerAsHtml 
+                    ? $header.html() 
+                    : $header.text()
+
+            ).on('click', tabClick);
+
+            $tabLi.append($tabLink);
+            $tabList.append($tabLi);
+
+            tabs.push($tabLi);
+        };
+
+        $tabListWrapper
+            .addClass(settings.classNames.tabItems);
+        
+        if ( settings.tabLabel)
+            $tabListWrapper.append($tabLabel);
+
+        $tabListWrapper
+            .append($tabList);
+
+        $container
+            .addClass(settings.classNames.tabs)
+            .prepend($tabListWrapper);
+
+        $('html').on('click', removeActiveState);
+
+        if ( settings.tabLabel)
+            $tabLabel.on('click', labelToggleActive);
+
+        $panels
+            .each(parsePanels)
+            .addClass(settings.classNames.content);
+
+        $(tabs[settings.activeIndex])
+            .find('a')
+            .click();
+
+        return {
+            'destroy': destroy
+        }
+    }
+
+    $.fn.contentSwitcher = function(args) {
+        var settings = jQuery.extend(true, defaults, args);
+        return this.each(function(){
+            if (undefined == $(this).data('contentSwitcher')) {
+                var plugin = new ContentSwitcher(this, settings);
+                $(this).data('contentSwitcher', plugin);
+            }
+        });
+    };
+
+})(jQuery);
