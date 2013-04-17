ac0f345e32642f3fd9d3e2ca6865297f84e7d670
OpenShift guest <dmmendez@gmail.com>
2013-04-16 23:28:27 -0400

    standalone updates

--- contentSwitcher.js
+++ contentSwitcher.js
@@ -12,21 +12,19 @@
       return destination;
     }
     
-    HTMLElement.prototype.hasClass = function(className) {
-        return new RegExp(' ' + className + ' ').test(' ' + this.className + ' ');
+    function hasClass(element, className) {
+        return new RegExp(' ' + className + ' ').test(' ' + element.className + ' ');
     };
     
-    HTMLElement.prototype.addClass = function(className) {
-        var elem = this;
-        if (!elem.hasClass(className)) {
+    function addClass(elem, className) {
+        if (!hasClass(elem, className)) {
             elem.className += ' ' + className;
         }
     };
     
-    HTMLElement.prototype.toggleClass = function(className) {
-        var elem = this;
+    function toggleClass(elem, className) {
         var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ' ) + ' ';
-        if (elem.hasClass(className)) {
+        if (hasClass(elem, className)) {
             while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
                 newClass = newClass.replace( ' ' + className + ' ' , ' ' );
             }
@@ -36,26 +34,48 @@
         }
     };
 
-    HTMLElement.prototype.removeClass = function(remove) {
+    function removeClass(elem, remove) {
         var newClassName = "";
         var i;
-        var classes = this.className.split(" ");
+        var classes = elem.className.split(" ");
         for(i = 0; i < classes.length; i++) {
             if(classes[i] !== remove) {
                 newClassName += classes[i] + " ";
             }
         }
-        this.className = newClassName;
+        elem.className = newClassName;
     };
     
-    HTMLElement.prototype.getChildren = function() {
-        var children = this.childNodes;
+    function getChildren(elem) {
+        var children = elem.childNodes;
         var _c = [];
         for ( var i=0; i < children.length; i++) {
-            if ( children[i].nodeType != 3 )
+            if ( children[i].nodeType === 1 )
                 _c.push(children[i]);
         }
         return _c;
+    }
+    
+    function getAllElementsWithAttribute(attribute, value)
+    {
+      var matchingElements = [];
+      var allElements = document.getElementsByTagName('*');
+      for (var i = 0; i < allElements.length; i++)
+      {
+          var attr = allElements[i].getAttribute(attribute);
+        if (attr && attr == value)
+        {            
+          // Element exists with attribute. Add to array.
+          matchingElements.push(allElements[i]);
+        }
+      }
+      return matchingElements;
+    }
+    
+    function each(items, callback) {
+        for ( var i=0; i < items.length; i++) {
+            callback.call(items[i], i);
+        }
     }
     
     function ContentSwitcher(container, options) {
@@ -84,7 +104,7 @@
             settings.classNames[key] = settings.prefix + settings.classNames[key];
         }
 
-        var panels = container.getChildren();
+        var panels = getChildren(container);
         var tabListWrapper = document.createElement('div');
         var tabList = document.createElement('ul');
         var tabLabel = document.createElement('a');
@@ -92,12 +112,12 @@
         var activeHeader;
 
         var removeActiveState = function() {
-            container.removeClass(settings.classNames.active);
+            removeClass(container, settings.classNames.active);
         };
 
         var labelToggleActive = function(event) {
             event.stopPropagation();
-            container.toggleClass(settings.classNames.active);
+            toggleClass(container, settings.classNames.active);
             return false;
         };
 
@@ -106,11 +126,76 @@
         };
         
         var parsePanel = function(panel, index) {
+            var header = panel.getElementsByTagName('h3')[0];
+            var id = panel.id;
+            var tabLink = document.createElement('a');
+            var tabLi = document.createElement('li');
+                        
+            tabLink.href = '#' + id;
+            tabLi.style.className = panel.style.className || '';
+
+            var isCurrentHeader = function() {
+                return (activeHeader != undefined) && activeHeader == index;
+            };
+            
+            var tabClick = function(event) {
+                var self = this;
+                var activeClass = settings.classNames.active;
+                var tabListLi = getChildren(tabList);
+                
+                each(panels, function() {
+                    removeClass(this, activeClass);
+                });
+                
+                if ( settings.toggleHeaders && !isCurrentHeader())
+                    removeClass(panel, settings.classNames.headers);
+                
+                each(tabListLi, function() {
+                    removeClass(this, activeClass);
+                });             
+
+                addClass(panel, activeClass);
+
+                addClass(tabLi, activeClass);
+                
+                if ( settings.tabLabel)
+                    tabLabel.innerText = self.innerText;
+
+                if(settings.onChange)
+                    settings.onChange.call(self, panel);
+
+                activeHeader = index;
+
+                return false;
+            };
+            
+            header.onclick = function() {
+                
+                if ( settings.toggleHeaders && isCurrentHeader())
+                    toggleClass(panel, settings.classNames.headers);
+
+                tabLink.click();
+            };
+
+            tabLink.innerHTML = settings.headerAsHtml ? header.innerHTML : header.innerText;
+            tabLink.onclick = tabClick;
+            
+            each(getAllElementsWithAttribute('data-toggle-content', id), function() {
+              this.onclick = function(event) {
+                  removeClass(panel, settings.classNames.headers);
+                  tabLink.click();
+                  return false;
+              };
+            });
+
+            tabLi.appendChild(tabLink);
+            tabList.appendChild(tabLi);
+
+            tabs.push(tabLi);
             
         }
 
-        tabListWrapper
-            .addClass(settings.classNames.tabItems);
+        addClass(tabListWrapper, settings.classNames.tabItems);
         
         if ( settings.tabLabel)
             tabListWrapper.appendChild(tabLabel);
@@ -118,7 +203,7 @@
         tabListWrapper
             .appendChild(tabList);
 
-        container.addClass(settings.classNames.tabs);
+        addClass(container, settings.classNames.tabs);
         container.insertBefore(tabListWrapper, container.firstChild);
 
         document.onclick = removeActiveState;
@@ -126,15 +211,12 @@
         if ( settings.tabLabel)
             tabLabel.onclick = labelToggleActive;
 
+        each(panels, function(i) {
+            parsePanel(this, i); 
+            addClass(this, settings.classNames.content);
+        });
         
-        for ( var i=0; i < panels.length; i++ ) {
-            parsePanel(panels[i]);
-            panels[i].addClass(settings.classNames.content);    
-        }        
-
-        //$(tabs[settings.activeIndex])
-          //  .find('a')
-            //.click();
+        tabs[settings.activeIndex].getElementsByTagName('a')[0].click();
 
         return {
             'destroy': destroy
